0e4382c9653ffed31571c99cf97c2c888e0c02fc
Anna <luchinina.anna@gmail.com>
2017-07-04 16:37:02 +0300

    Add function loadResources

--- Jump 'n Bump/js/main.js
+++ Jump 'n Bump/js/main.js
@@ -3,27 +3,65 @@ g_world.ctx = document.getElementById("canvas").getContext("2d");
 g_world.prevTime = new Date();
 g_world.colors = new Colors();
 g_world.resources = createResourceStore();
+g_world.storeImages = new Store();
 
-loadOfResources();
+loadResources();
 
-function loadOfResources() {
-  let resourceCache = {};
-  let loading = [];
-  let readyCallbacks = [];
+function loadResources() {
   let resources = g_world.resources;
-  if (resources instanceof Array)
-  for (let i = 0; i < resources.length; i++) {
-    loadImage(resources.imageUrl, resourceCache)
+  let storeImages = g_world.storeImages;
+  if (resources instanceof Array) {
+    for (let i = 0; i < resources.length; i++) {
+      load(resources.imageUrl, storeImages)
+    }
+  }
+  else {
+    load(resources, storeImages)
   }
 }
 
-function loadImage(url, resourceCache){}
+function load(url, storeImages) {
+  if (storeImages.resourceCache[url]) {
+    return storeImages;
+  } else {
+    let img = new Image();
+    img.onload = function () {
+      storeImages.resourceCache[url] = img;
+      if (isReady(storeImages.resourceCache)) {
+        for (let j = 0; j < storeImages.readyCallbacks.length; j++) {
+          loadResources(func) {
+            func();
+          }
+        }
+      }
+      img.src = url;
+      storeImages.resourceCache[url] = false;
+      return storeImages;
+    }
+  }
+}
 
-//window.requestAnimationFrame(gameLoop);
+function isReady(resourceCache) {
+  let ready = true;
+  for(let k in resourceCache) {
+    if((resourceCache.hasOwnProperty(k)) && (!resourceCache[k])) {
+      ready = false;
+    }
+  }
+  return ready;
+}
+function onReady(func) {
+  g_world.storeImages.readyCallbacks.push(func);
+}
+function get(url) {
+  return resourceCache[url];
+}
 
-//gameLoop();
+g_world.storeImages.onReady(gameLoop);
 
-//function gameLoop() {
+window.requestAnimationFrame(gameLoop);
+
+function gameLoop() {
   let ctx = g_context.ctx;
   let currTime = new Date();
   let prevTime = g_world.prevTime;
@@ -34,32 +72,36 @@ function loadImage(url, resourceCache){}
   requestAnimationFrame(gameLoop);
   g_world.prevTime = currTime;
 }
-
 function update() {}
 function cleanCanvas(ctx) {
   ctx.fillStyle = g_world.colors.WHITE;
   drawRectangle(ctx, 0, 0, 1200, 900, 1, 0, 0);
 }
 function render(ctx) {}
-
 function drawRectangle(ctx, x, y, width, height, amount, shiftRight, shiftDown) {
   for (let i = 0; i < amount; i++) {
     ctx.fillRect(x + i * shiftRight, y + i * shiftDown, width, height);
     ctx.strokeRect(x + i * shiftRight, y + i * shiftDown, width, height);
   }
 }
-
 function Colors() {
-  this.WHITE = "#FFF9FF";
+  this.WHITE = "#FFFFFF";
 }
-function Image(name, url) {
-  this.imageName = name;
+
+function Image(url) {
   this.imageUrl = url;
 }
-
 function createResourceStore() {
   resources = [];
-  resources[0] = new Image("earth", "https://opengameart.org/sites/default/files/spr_tileset_pre_0.png");
-  resources[1] = new Image("iceAndWater", "https://opengameart.org/sites/default/files/preview_154.png");
-  return resources
+  resources[0] = new Image("https://opengameart.org/sites/default/files/spr_tileset_pre_0.png");
+  resources[1] = new Image("https://opengameart.org/sites/default/files/preview_154.png");
+  return resources;
+}
+function Store() {
+  this.resourceCache = {};
+  this.readyCallbacks = [];
+  this.load = load;
+  this.get = get;
+  this.onReady = onReady;
+  this.isReady = isReady;
 }
